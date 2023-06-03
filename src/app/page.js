6b343d3dc0cee933dd7cc2e29b8153133a38e03a
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-06-03 13:12:46 -0400

    summary section and assets

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { sriracha, merriweather } from './layout.js';
 
+import profilePhoto from "./assets/Oley_Matthew_profile.png"
+
 export default function Home() {
   return (
     <html className={[sriracha.variable, merriweather.variable].join(" ")}>
@@ -9,6 +11,7 @@ export default function Home() {
         <div className={styles.main}>
           <Title first="Matthew" last="Oley" />
           <Divider announces="summary" comesBefore="1" />
+          <Summary />
         </div>
       </body>
     </html>
@@ -18,12 +21,11 @@ export default function Home() {
 export function Divider( {announces, comesBefore} ) {
   const prevIndex = parseInt(comesBefore) - 1;
   const nextIndex = parseInt(comesBefore);
-  let divider = (
+  return (
     <svg id={announces} className={styles.divider} viewBox='0 0 1000 100' preserveAspectRatio='none' style={{backgroundColor: "var(--main-colors-"+prevIndex+")"}}>
       <polygon x="1" y="1" points='0,100 300,0 500,50 700,0 1000,100' style={{fill: "var(--main-colors-"+nextIndex+")"}} />
       </svg>
   );
-  return divider
 }
 
 export function Title( {first, last} ) {
@@ -32,6 +34,26 @@ export function Title( {first, last} ) {
       <div style={{display: "flex", position: "relative", width: "80%", left: "10%", right: "10%", margin: "100px 0px 0px 0px", padding: "15px", justifyContent: "center", flexDirection: "column"}}>
           <span style={{fontSize: "100pt", fontFamily: "var(--font-sriracha)", textAlign: "center"}}>{first}</span>
           <span style={{fontSize: "50pt", fontFamily: "var(--font-sriracha)", textAlign: "center", transform: "translateY(-50pt)"}}>{last}</span>
+      </div>
+    </div>
+  )
+}
+
+export function Summary() {
+  return (
+    <div style={{width: "100%", margin: "0px 0px -1px 0px", backgroundColor: "var(--main-colors-1)", display: "flex", justifyContent: "center"}}>
+      <div style={{display: "flex", position: "relative", width: "max-content", maxWidth: "80%", margin: "70px 0px 100px 0px", flexWrap: "wrap", padding: "30px", backgroundColor: "#71c1de", borderRadius: "27px", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
+        <Image src={profilePhoto} alt='profile picture' className={styles.circle_picture} />
+        <div style={{width: "calc(100% - 300px)"}}>
+          <div className={styles.body}>
+            <ul>
+              <li data-icon="major" className={styles.list}>BS Software Engineering</li>
+              <li data-icon="location" className={styles.list}>Dallas, Pennsylvania</li>
+              <li data-icon="school" className={styles.list}>Rochester Institute of Technology</li>
+              <li data-icon="gpa" className={styles.list}>3.85 GPA</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
